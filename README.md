464185ff4d48c91efba502b2c4700025eb5dd2ea
XWeb-CoderX <rileytheangel75@gmail.com>
2023-06-01 20:12:18 +0100

    add: css

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Available Scripts
+## Available Scripts npx create-react-app
 
 In the project directory, you can run:
 
--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [riddle, setRiddle] = useState({ question: "", answer: "" });
+
+  const getRiddles = async () => {
+    let data = await fetch("https://api.api-ninjas.com/v1/riddles");
+    let convertedData = await data.json();
+    console.log(convertedData);
+    setRiddle(convertedData[0]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Your riddle will appear here:</h1>
+        <button onClick={getRiddles} className="riddlebutton">
+          Click here to get riddle
+        </button>
+        <h3>{riddle.question}</h3>
+        <h1 className="arrow"> ⬇ </h1>
+        <h3 className="correct-answer">{riddle.answer}</h3>
+      </div>
     </div>
   );
 }
-
-export default App;
